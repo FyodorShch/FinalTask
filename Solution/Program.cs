40f20f248befb2b76e7de2c7663a81ea1800b3f7
Fyodor <feodorinin@gmail.com>
2023-02-14 20:48:36 +0300

    Добавил код решения задачи

---
+++ Solution/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите строку");
+string[] StringArray = CreateArray(Console.ReadLine());
+Console.WriteLine();
+ShowString(NewArray(StringArray));
+
+string[] CreateArray(string ss)
+{
+    string[] words = ss.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    string[] result = new string[words.Length];
+    for (int i = 0; i < words.Length; i++)
+    {
+        result[i] = words[i];
+    }
+    return result;
+}
+void ShowString(string[] s)
+{
+    Console.Write("[");
+    for (int j = 0; j < s.Length; j++)
+    {
+        Console.Write($"{s[j]}");
+    }
+    Console.Write("]");
+}
+string[] NewArray(string[] array)
+{
+    int w = 0;
+    string[] s = new string[array.Length];
+    for (int q = 0; q < s.Length; q++)
+    {
+        if (array[q].Length <= 3)
+        {
+            s[w] = array[q];
+            w++;
+        }
+    }
+    return s;
+}
